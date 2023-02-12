aac028915f79a926c3b5442b9cc5329cc43e5a66
Di <hanwi1994@gmail.com>
2023-02-12 13:22:01 +0100

    add chai into deps

--- SpinWords.js
+++ SpinWords.js
@@ -23,53 +23,24 @@ function spinWords(words){
   }).join(' ');
 }
 */
+console.log(spinWords("Welcome"));
 
-//Tests from codewars:
+// Tests from codewars:
 const chai = require("chai");
 const assert = chai.assert;
-
-describe("Spinning words", () => {
-  function dotest(input, expected) {
-    assert.strictEqual(
-      spinWords(input),
-      expected,
-      `Incorrect answer for input="${input}"`
-    );
-  }
-
-  it("Fixed tests", () => {
-    dotest("Welcome", "emocleW");
-    dotest("Hey fellow warriors", "Hey wollef sroirraw");
-    dotest("This is a test", "This is a test");
-    dotest("This is another test", "This is rehtona test");
-    dotest("This sentence is a sentence", "This ecnetnes is a ecnetnes");
-    dotest(
-      "You are almost to the last test",
-      "You are tsomla to the last test"
-    );
-    dotest(
-      "Just kidding there is still one more",
-      "Just gniddik ereht is llits one more"
-    );
-    dotest("Seriously this is the last one", "ylsuoireS this is the last one");
-  });
-
-  it("Random tests", () => {
-    const refSpinWords = (s) =>
-      s
-        .split(" ")
-        .map((s) => (s.length >= 5 ? s.split("").reverse().join("") : s))
-        .join(" ");
-    const rnd = (n) => Math.floor(Math.random() * n);
-    const abc = "abcdefghijklmnopqrstuvwxyz";
-    const rndWord = () =>
-      Array.from({ length: rnd(12) + 1 }, () => abc[rnd(abc.length)]).join("");
-    const rndSentence = (i) =>
-      Array.from({ length: rnd(i) + 1 }, rndWord).join(" ");
-    for (let i = 1; i <= 50; i++) {
-      const s = rndSentence(i);
-      const expected = refSpinWords(s);
-      dotest(s, expected);
-    }
-  });
-});
+assert.strictEqual(spinWords("Welcome"), "emocleW");
+assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
+assert.strictEqual(spinWords("This is a test"), "This is a test");
+assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
+assert.strictEqual(
+  spinWords("You are almost to the last test"),
+  "You are tsomla to the last test"
+);
+assert.strictEqual(
+  spinWords("Just kidding there is still one more"),
+  "Just gniddik ereht is llits one more"
+);
+assert.strictEqual(
+  spinWords("Seriously this is the last one"),
+  "ylsuoireS this is the last one"
+);
