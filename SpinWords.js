bac663e594af0d79dae908e5199f11332949207e
Di <hanwi1994@gmail.com>
2023-02-25 08:45:53 +0100

    Move tests for spinWords into a separate file

--- SpinWords.js
+++ SpinWords.js
@@ -1,7 +1,7 @@
 /**
  * @param {string} string event
  */
-function spinWords(string) {
+export function spinWords(string) {
   const myArray = string.split(" ");
   let newArray = myArray.map(transform); // map returns result of func, (forEach not)
 
@@ -23,24 +23,3 @@ function spinWords(words){
   }).join(' ');
 }
 */
-console.log(spinWords("Welcome"));
-
-// Tests from codewars:
-const chai = require("chai");
-const assert = chai.assert;
-assert.strictEqual(spinWords("Welcome"), "emocleW");
-assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
-assert.strictEqual(spinWords("This is a test"), "This is a test");
-assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
-assert.strictEqual(
-  spinWords("You are almost to the last test"),
-  "You are tsomla to the last test"
-);
-assert.strictEqual(
-  spinWords("Just kidding there is still one more"),
-  "Just gniddik ereht is llits one more"
-);
-assert.strictEqual(
-  spinWords("Seriously this is the last one"),
-  "ylsuoireS this is the last one"
-);
